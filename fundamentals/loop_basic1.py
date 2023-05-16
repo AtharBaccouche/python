63cfc95a2b792a67f9684c8260d5384384776a3b
AtharBaccouche <baccouche.athar@gmail.com>
2023-05-16 09:28:17 +0200

    first commit

---
+++ fundamentals/loop_basic1.py
@@ -0,0 +1,11 @@
+# for i in range(151):
+#     print(i)
+
+for i in range(5,1001,5):
+    print(i)
+
+for i in range(1, 101):
+    if (i%5==0):
+        print("codding")
+    else:
+        print("coding dojo")
